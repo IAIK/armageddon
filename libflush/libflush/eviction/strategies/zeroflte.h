0e78d9ee16d61a356184e09903771f5fcd0f274a
Moritz Lipp <mail@mlq.me>
2016-10-31 21:01:50 +0100

    Update eviction strategies for zeroflte and mako

--- libflush/libflush/eviction/strategies/zeroflte.h
+++ libflush/libflush/eviction/strategies/zeroflte.h
@@ -1,9 +1,19 @@
 /* See LICENSE file for license and copyright information */
 
+/* Samsung S6 (A53) */
+#if 1
+#define NUMBER_OF_SETS 512
+#define LINE_LENGTH_LOG2 6
+#define LINE_LENGTH 64
+#define ES_EVICTION_COUNTER 21
+#define ES_NUMBER_OF_ACCESSES_IN_LOOP 2
+#define ES_DIFFERENT_ADDRESSES_IN_LOOP 5
+#else
 /* Samsung S6 (A57) */
 #define NUMBER_OF_SETS 2048
 #define LINE_LENGTH_LOG2 6
 #define LINE_LENGTH 64
-#define ES_EVICTION_COUNTER 3
-#define ES_NUMBER_OF_ACCESSES_IN_LOOP 8
-#define ES_DIFFERENT_ADDRESSES_IN_LOOP 8
+#define ES_EVICTION_COUNTER 25
+#define ES_NUMBER_OF_ACCESSES_IN_LOOP 10
+#define ES_DIFFERENT_ADDRESSES_IN_LOOP 10
+#endif
