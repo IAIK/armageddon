96ebc2d86e0454947a6cb2345cb93e984f5f6d39
Daniel Gruss <lava@gruss.cc>
2017-10-07 11:58:07 +0200

    Add comment for assertion
    Add comment for assertion to make code more self-explaining

--- libflush/libflush/timing.c
+++ libflush/libflush/timing.c
@@ -48,7 +48,7 @@ perf_init(libflush_session_t* session, libflush_session_args_t* args)
   attr.exclude_callchain_kernel = 1;
 
   session->perf.fd = syscall(__NR_perf_event_open, &attr, 0, -1, -1, 0);
-  assert(session->perf.fd >= 0);
+  assert(session->perf.fd >= 0 && "if this assertion fails you have no perf event interface available for the userspace. install a different kernel/rom."); // if this assertion fails you have no perf event interface available for the userspace. install a different kernel/rom.
 
   return true;
 }
